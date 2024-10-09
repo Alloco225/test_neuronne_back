42192adf11703b4db28b7dba5eb339a5491ba519
Alloco + Ketchup <amanehosanna@gmail.com>
2024-10-09 10:34:51 +0000

    💥 NEW: created Post model, migration, factory, seed

---
+++ app/Models/Post.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Post extends Model
+{
+    /** @use HasFactory<\Database\Factories\PostFactory> */
+    use HasFactory;
+
+    protected $guarded = [];
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'last_update';
+
+    protected static function booted(): void
+    {
+        // * Mettre à jour le slug automatiquement à partir du titre
+        // static::created(function (Post $post) {
+        static::updated(function (Post $post) {
+            $post->slug = Str::slug($post->title);
+        });
+    }
+
+    // * image_url attribut calculé
+    public function getImageUrlAttribute(){
+        return $this->image_path;
+    }
+
+}
