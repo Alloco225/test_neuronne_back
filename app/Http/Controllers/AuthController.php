78bca4db97f05ca71976d010ad792249f402f322
Alloco + Ketchup <amanehosanna@gmail.com>
2024-10-09 12:49:08 +0000

    💥 NEW: Authcontroller, register, login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,7 +4,82 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Hash;
+use Illuminate\Validation\ValidationException;
+
 class AuthController extends Controller
 {
     //
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'age' => 'required|string|max:15|unique:users',
+            'picture_file' => "required|file|mimes:png,jpg",
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        $picturePath = null;
+        if ($request->hasFile('image_file')) {
+            $image = $request->file('image_file');
+            $path = $image->storeAs('public/users', $request->email. $image->getClientOriginalExtension());
+            $picturePath = str_replace('public', '', $path);
+        }
+
+        $user = User::create([
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'email' => $request->email,
+            'age' => $request->age,
+            'picture_path' => $picturePath,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = $user->createToken('auth-token')->plainTextToken;
+
+        return response()->json([
+            'token' => $token,
+            'user' => $user,
+        ]);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'password' => [__('auth.failed')],
+                // 'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        $token = $user->createToken('auth-token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return response()->json(true);
+    }
+
+    public function user(Request $request)
+    {
+        return response()->json($request->user());
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,9 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('auth')->group(function () {
     Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
-    Route::delete('logout', [AuthController::class, 'logout']);
+    Route::delete('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 });
 
-Route::resource('posts', PostController::class);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::resource('posts', PostController::class);
+});
