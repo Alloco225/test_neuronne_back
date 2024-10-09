7f12c86da63e7ed23516fe8611ddb8ac080f9e56
Alloco + Ketchup <amanehosanna@gmail.com>
2024-10-09 11:21:42 +0000

    💥 NEW: post index

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,9 +10,42 @@ class PostController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $user = $request->user();
+
+        $search = $request->q;
+        $limit = $request->limit;
+        $skip = $request->skip;
+        $select = $request->select;
+        $sortBy = $request->sortBy;
+        $order = $request->order ?? 'desc';
+
+        // $posts =  Post::when($search, function($query, $search){
+        $posts = $user->posts->when($search, function($query, $search){
+            return $query
+            ->where('title', 'like', "%$search%")
+            ->orWhere('slug', 'like', "%$search%")
+            ->orWhere('content', 'like', "%$search%");
+        })
+        ->when($sortBy, function($query, $sortBy) use ($order){
+            return $query
+            ->when($order, function($query, $order) use ($sortBy){
+                $query->sortBy($sortBy, $order);
+            });
+        }, function($query){
+            return $query->latest();
+        })
+        ->when($select, function($query, $select){
+            return $query->select($select);
+        })
+        ->when($skip, function($query, $skip){
+            return $query->skip($skip);
+        })
+        ->when($limit, function($query, $limit){
+            return $query->limit($limit);
+        });
+        return response()->json($posts);
     }
 
     /**
@@ -28,7 +61,22 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
+        $validatedData = $request->validate([
+            'title' => "required|string",
+            'content' => "required",
+            'image_file' => "required|file|mimes:png,jpg",
+        ]);
         //
+        $imagePath = null;
+        if($request->hasFile('image_file')){
+
+        }
+        $post = Post::create([
+            'title' => $validatedData['title'],
+            'content' => $validatedData['content'],
+        ]);
+
+        return response()->json($post);
     }
 
     /**
@@ -37,6 +85,9 @@ class PostController extends Controller
     public function show(Post $post)
     {
         //
+
+        return response()->json($post);
+
     }
     public function getPostBySlug(Post $post)
     {
@@ -65,5 +116,7 @@ class PostController extends Controller
     public function destroy(Post $post)
     {
         //
+        $post->delete();
+        return response()->json(true);
     }
 }
--- database/migrations/2024_10_09_102521_create_posts_table.php
+++ database/migrations/2024_10_09_102521_create_posts_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('title');
             $table->string('slug')->unique();
             $table->text('content');
-            $table->string('image_path');
+            $table->string('image_path')->nullable();
             $table->timestamps();
         });
     }
